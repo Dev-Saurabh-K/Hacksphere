261def99c31edde52d59c10fda19cf877768fe25
KK057 <bky8422@gmail.com>
2026-05-29 13:32:14 +0530

    Landing_page

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import ClassSelection from "./classSelection";
 import Login from "./Pages/Authentication/Login";
 import Signup from "./Pages/Authentication/Signup";
 import Dashboard from "./Pages/Dashboard/page2";
+import Landing_page from "./Landing_page"
 
 function App() {
 
   return (
     <>
     <Routes> 
-      <Route path="/dashboard" element={<Dashboard/>}/>
-    <Route path="/" element={<Login/>}/>
+    <Route path="/" element={<Landing_page/>}/>
+    <Route path="/dashboard" element={<Dashboard/>}/>
+    <Route path="/login" element={<Login/>}/>
     <Route path="/classSelection" element={<ClassSelection/>}/>
     <Route path="/signup" element={<Signup/>}/>
     </Routes>
